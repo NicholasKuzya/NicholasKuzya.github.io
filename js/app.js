ad6c7e769409d9d39f15e7c479d6d5e985093969
NicholasKuzya <kuzievkolaf@gmail.com>
2021-11-21 16:04:08 +0500

    Add Показать/Скрыть пункты function

--- js/app.js
+++ js/app.js
@@ -49,3 +49,67 @@ $(document).ready(function () {
         return false;
     });
 });
+$(document).ready(function(){
+    $('.b-points-toggle.one').click(function(){
+        var active = $(this).attr("data-text-after");
+        var notActive = $(this).attr("data-text-before");
+        var count = $(this).attr("data-click-count"); 
+        if(count == 1){
+            $(".b-points-toggle.one").addClass('text-active')
+        }
+        if('.text-active'){
+            $(".text-active").text(active);
+            $(this).attr("data-click-count", "2")
+        }
+        if(count == 2){
+            $('.b-points-toggle').removeClass('text-active')
+            $(this).text(notActive)
+            $(this).attr("data-click-count", "1")
+        }
+    })
+})
+$(document).ready(function(){
+    $('.b-points-toggle.two').click(function(){
+        var active = $(this).attr("data-text-after");
+        var notActive = $(this).attr("data-text-before");
+        var count = $(this).attr("data-click-count"); 
+        if(count == 1){
+            $(".b-points-toggle.two").addClass('text-active')
+        }
+        if('.text-active'){
+            $(".text-active").text(active);
+            $(this).attr("data-click-count", "2")
+        }
+        if(count == 2){
+            $('.b-points-toggle').removeClass('text-active')
+            $(this).text(notActive)
+            $(this).attr("data-click-count", "1")
+        }
+    })
+})
+$(document).ready(function(){
+    $('.b-points-toggle.three').click(function(){
+        var active = $(this).attr("data-text-after");
+        var notActive = $(this).attr("data-text-before");
+        var count = $(this).attr("data-click-count"); 
+        if(count == 1){
+            $(".b-points-toggle.three").addClass('text-active')
+        }
+        if('.text-active'){
+            $(".text-active").text(active);
+            $(this).attr("data-click-count", "2")
+        }
+        if(count == 2){
+            $('.b-points-toggle').removeClass('text-active')
+            $(this).text(notActive)
+            $(this).attr("data-click-count", "1")
+        }
+    })
+})
+/* 
+Смотри
+Тебе надо создать в html 2 атрибута
+Засунуть в один текст до активности, а в другой после активности.
+При нажатии на кнопку дать класс актив, и в него условие, если Актив, то возьми текст из нужного атрибута и замени текст главный.
+А если нет актива, то возьми текст с другого атрибута и замени главный текст.
+*/
